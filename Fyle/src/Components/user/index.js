3c4ec02f743290a206abb4a0e105bfeb3de1e807
Saurabh Kumar singh <pcsingh050301@gmail.com>
2022-04-12 01:21:42 +0530

    fyle

---
+++ Fyle/src/Components/user/index.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+const index = () => {
+    useEffect(() => {
+      getRepos()=>{
+
+      }
+    
+      return () => {
+        
+      }
+    }, [repos])
+    
+  return (
+    <div></div>
+  )
+}
+
+export default index
